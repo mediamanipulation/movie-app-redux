14afd9c695b5f9866d2ae32772bebb62aab3f8c8
Craig Mcspadden <enigma@Craigs-MacBook-Pro.local>
2020-06-25 14:47:44 -0500

    changed movie.jd to functional component

---
+++ src/components/containers/MovieContainer.js
@@ -0,0 +1,58 @@
+componentDidMount() {
+    // ES6 destructuring the props
+    const { movieId } = this.props.match.params;
+
+    if (localStorage.getItem(`${movieId}`)) {
+      let state = JSON.parse(localStorage.getItem(`${movieId}`))
+      this.setState({ ...state })
+    } else {
+      this.setState({ loading: true })
+      // First fetch the movie ...
+      let endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`;
+      this.fetchItems(endpoint);
+    }
+  }
+
+  fetchItems = (endpoint) => {
+    // ES6 destructuring the props
+    const { movieId } = this.props.match.params;
+
+    fetch(endpoint)
+    .then(result => result.json())
+    .then(result => {
+
+      if (result.status_code) {
+        // If we don't find any movie
+        this.setState({ loading: false });
+      } else {
+        this.setState({ movie: result }, () => {
+          // ... then fetch actors in the setState callback function
+          let endpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+          fetch(endpoint)
+          .then(result => result.json())
+          .then(result => {
+
+            const directors = result.crew.filter( (member) => member.job === "Director");
+
+            this.setState({
+              actors: result.cast,
+              directors,
+              loading: false
+            }, () => {
+              localStorage.setItem(`${movieId}`, JSON.stringify(this.state));
+            })
+          })
+        })
+      }
+    })
+    .catch(error => console.error('Error:', error))
+  }
+
+// state from movie.js
+
+//     state = {
+//       movie: null,
+//       actors: null,
+//       directors: [],
+//       loading: false
+//     }
